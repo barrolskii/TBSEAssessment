66713b16d517beda4941154603fe0746d60e3e81
Barrolskii <ajbtdc@gmail.com>
2019-03-11 16:43:46 +0000

    Added loading code into the main form. Still needs work but data is read from CSV file successfully and stored correctly

--- TBSEAssessmentOne/TBSEAssessmentOne/Form1.Designer.cs
+++ TBSEAssessmentOne/TBSEAssessmentOne/Form1.Designer.cs
@@ -29,11 +29,17 @@
 		private void InitializeComponent()
 		{
 			this.button1 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(12, 396);
+			this.button1.Location = new System.Drawing.Point(791, 110);
+			this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(145, 42);
 			this.button1.TabIndex = 0;
@@ -41,21 +47,67 @@
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+			this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(311, 545);
+			this.richTextBox1.TabIndex = 1;
+			this.richTextBox1.Text = "";
+			this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(340, 110);
+			this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(435, 198);
+			this.dataGridView1.TabIndex = 2;
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Location = new System.Drawing.Point(340, 12);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.Size = new System.Drawing.Size(735, 73);
+			this.richTextBox2.TabIndex = 3;
+			this.richTextBox2.Text = "";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(835, 524);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(240, 22);
+			this.textBox1.TabIndex = 4;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(1087, 568);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.richTextBox2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.richTextBox1);
 			this.Controls.Add(this.button1);
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.TextBox textBox1;
 	}
 }
 
--- TBSEAssessmentOne/TBSEAssessmentOne/Form1.cs
+++ TBSEAssessmentOne/TBSEAssessmentOne/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace TBSEAssessmentOne
 {
@@ -15,19 +17,101 @@ namespace TBSEAssessmentOne
 		public Form1()
 		{
 			InitializeComponent();
+
+			dataGridView1.ColumnCount = 2;
+			dataGridView1.Columns[0].Name = "StoreCode";
+			dataGridView1.Columns[1].Name = "StoreName";
 		}
 
 		private void button1_Click(object sender, EventArgs e)
 		{
 			OpenFileDialog openFileDialog = new OpenFileDialog();
-			openFileDialog.InitialDirectory = System.IO.Directory.GetCurrentDirectory() + "\\StoreData";
+			openFileDialog.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
 			openFileDialog.Filter = "CSV files|*.csv";
 			openFileDialog.Title = "File";
 
 			if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
 			{
-				MessageBox.Show(openFileDialog.FileName);
+				MessageBox.Show("foo");
+				Stopwatch stopwatch = new Stopwatch();
+				stopwatch.Start();
+				richTextBox2.Text = openFileDialog.FileName;
+
+				Dictionary<string, Store> Stores = new Dictionary<string, Store>();
+				HashSet<Date> dates = new HashSet<Date>();
+				List<Order> orders = new List<Order>();
+
+				string[] storeList = File.ReadAllLines(openFileDialog.FileName);
+
+				foreach (var s in storeList)
+				{
+					string[] splitStoreData = s.Split(',');
+
+					Store newStore = new Store { storeCode = splitStoreData[0], storeLocation = splitStoreData[1] };
+					if (!Stores.ContainsKey(newStore.storeCode))
+						Stores.Add(newStore.storeCode, newStore);
+				}
+
+				string[] fileNames = Directory.GetFiles(openFileDialog.InitialDirectory + @"/StoreData");
+				foreach (var filePath in fileNames)
+				{
+					string fileNameExt = Path.GetFileName(filePath);
+					string fileName = Path.GetFileNameWithoutExtension(filePath);
+
+					string[] fileNameSplit = fileName.Split('_');
+					Store store = Stores[fileNameSplit[0]];
+					Date date = new Date { week = Convert.ToInt32(fileNameSplit[1]), year = Convert.ToInt32(fileNameSplit[2]) };
+					dates.Add(date);
+
+					string[] orderData = File.ReadAllLines(openFileDialog.InitialDirectory + @"/StoreData/" + fileNameExt);
+					foreach (var orderInfo in orderData)
+					{
+						string[] orderSplit = orderInfo.Split(',');
+						Order order = new Order
+						{
+							store = store,
+
+							date = date,
+							supplier = orderSplit[0],
+							supplierType = orderSplit[1],
+							cost = Convert.ToDouble(orderSplit[2])
+						};
+
+						orders.Add(order);
+					}
+				}
+
+
+				stopwatch.Stop();
+				textBox1.Text += "Time to load: " + stopwatch.Elapsed;
 			}
+		}
+
+		class Store
+		{
+			public string storeCode { get; set; }
+			public string storeLocation { get; set; }
+		}
+
+		class Order
+		{
+			public Store store { get; set; }
+
+			public Date date { get; set; }
+			public string supplier { get; set; }
+			public string supplierType { get; set; }
+			public double cost { get; set; }
+		}
+
+		class Date
+		{
+			public int week { get; set; }
+			public int year { get; set; }
+		}
+
+		private void richTextBox1_TextChanged(object sender, EventArgs e)
+		{
+			
 		}
 	}
 }
