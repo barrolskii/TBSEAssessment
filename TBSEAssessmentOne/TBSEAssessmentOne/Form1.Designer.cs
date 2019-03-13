e911983afb3340873582fe7f8455bffb52e6ce69
Barrolskii <ajbtdc@gmail.com>
2019-03-13 20:53:59 +0000

    Added more functionality to the winforms application, added the ability to search for individual stores and removed the csv loader class

--- TBSEAssessmentOne/TBSEAssessmentOne/Form1.Designer.cs
+++ TBSEAssessmentOne/TBSEAssessmentOne/Form1.Designer.cs
@@ -33,12 +33,25 @@
 			this.dataGridView1 = new System.Windows.Forms.DataGridView();
 			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
 			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.comboBox3 = new System.Windows.Forms.ComboBox();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(791, 110);
+			this.button1.Location = new System.Drawing.Point(1081, 12);
 			this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(145, 42);
@@ -52,7 +65,7 @@
 			this.richTextBox1.Location = new System.Drawing.Point(12, 12);
 			this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.richTextBox1.Name = "richTextBox1";
-			this.richTextBox1.Size = new System.Drawing.Size(311, 545);
+			this.richTextBox1.Size = new System.Drawing.Size(313, 310);
 			this.richTextBox1.TabIndex = 1;
 			this.richTextBox1.Text = "";
 			this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
@@ -60,11 +73,11 @@
 			// dataGridView1
 			// 
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(340, 110);
+			this.dataGridView1.Location = new System.Drawing.Point(6, 5);
 			this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.dataGridView1.Name = "dataGridView1";
 			this.dataGridView1.RowTemplate.Height = 24;
-			this.dataGridView1.Size = new System.Drawing.Size(435, 198);
+			this.dataGridView1.Size = new System.Drawing.Size(672, 201);
 			this.dataGridView1.TabIndex = 2;
 			// 
 			// richTextBox2
@@ -77,25 +90,120 @@
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(835, 524);
+			this.textBox1.Location = new System.Drawing.Point(1038, 377);
 			this.textBox1.Name = "textBox1";
 			this.textBox1.Size = new System.Drawing.Size(240, 22);
 			this.textBox1.TabIndex = 4;
+			// 
+			// richTextBox3
+			// 
+			this.richTextBox3.Location = new System.Drawing.Point(1152, 91);
+			this.richTextBox3.Name = "richTextBox3";
+			this.richTextBox3.Size = new System.Drawing.Size(126, 280);
+			this.richTextBox3.TabIndex = 5;
+			this.richTextBox3.Text = "";
+			// 
+			// dataGridView2
+			// 
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView2.Location = new System.Drawing.Point(3, 6);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.RowTemplate.Height = 24;
+			this.dataGridView2.Size = new System.Drawing.Size(675, 198);
+			this.dataGridView2.TabIndex = 6;
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Location = new System.Drawing.Point(340, 91);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(692, 560);
+			this.tabControl1.TabIndex = 7;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.dataGridView1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 25);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(684, 531);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Store List";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.dataGridView2);
+			this.tabPage2.Location = new System.Drawing.Point(4, 25);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(684, 531);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Store Data";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(12, 359);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(176, 24);
+			this.comboBox1.TabIndex = 8;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 533);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(141, 50);
+			this.button2.TabIndex = 9;
+			this.button2.Text = "Search store";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(12, 404);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(176, 24);
+			this.comboBox2.TabIndex = 10;
+			// 
+			// comboBox3
+			// 
+			this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox3.FormattingEnabled = true;
+			this.comboBox3.Location = new System.Drawing.Point(13, 450);
+			this.comboBox3.Name = "comboBox3";
+			this.comboBox3.Size = new System.Drawing.Size(175, 24);
+			this.comboBox3.TabIndex = 11;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1087, 568);
+			this.ClientSize = new System.Drawing.Size(1290, 663);
+			this.Controls.Add(this.comboBox3);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.tabControl1);
+			this.Controls.Add(this.richTextBox3);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.richTextBox2);
-			this.Controls.Add(this.dataGridView1);
 			this.Controls.Add(this.richTextBox1);
 			this.Controls.Add(this.button1);
 			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -108,6 +216,15 @@
 		private System.Windows.Forms.DataGridView dataGridView1;
 		private System.Windows.Forms.RichTextBox richTextBox2;
 		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.RichTextBox richTextBox3;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.ComboBox comboBox3;
 	}
 }
 
--- TBSEAssessmentOne/TBSEAssessmentOne/Form1.cs
+++ TBSEAssessmentOne/TBSEAssessmentOne/Form1.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
@@ -14,13 +16,31 @@ namespace TBSEAssessmentOne
 {
 	public partial class Form1 : Form
 	{
+		string storeCSVFileLocation;
+
 		public Form1()
 		{
 			InitializeComponent();
 
+			comboBox1.Enabled = false;
+			comboBox2.Enabled = false;
+			comboBox3.Enabled = false;
+
+			/* Populate the second combo box to have a selection of weeks */
+			for (int i = 1; i <= 52; i++)
+				comboBox2.Items.Add(i);
+
+			/* Only two years to choose from in the files so this combo box
+			 * only needs two options
+			 */
+			comboBox3.Items.Add(2013);
+			comboBox3.Items.Add(2014);
+
 			dataGridView1.ColumnCount = 2;
 			dataGridView1.Columns[0].Name = "StoreCode";
 			dataGridView1.Columns[1].Name = "StoreName";
+
+			dataGridView2.ColumnCount = 3;
 		}
 
 		private void button1_Click(object sender, EventArgs e)
@@ -30,11 +50,10 @@ namespace TBSEAssessmentOne
 			openFileDialog.Filter = "CSV files|*.csv";
 			openFileDialog.Title = "File";
 
+			int fileCount = 0;
+
 			if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
 			{
-				MessageBox.Show("foo");
-				Stopwatch stopwatch = new Stopwatch();
-				stopwatch.Start();
 				richTextBox2.Text = openFileDialog.FileName;
 
 				Dictionary<string, Store> Stores = new Dictionary<string, Store>();
@@ -50,40 +69,44 @@ namespace TBSEAssessmentOne
 					Store newStore = new Store { storeCode = splitStoreData[0], storeLocation = splitStoreData[1] };
 					if (!Stores.ContainsKey(newStore.storeCode))
 						Stores.Add(newStore.storeCode, newStore);
+
+					dataGridView1.Rows.Add(newStore.storeCode, newStore.storeLocation);
+					comboBox1.Items.Add(newStore.storeCode);
 				}
 
+				/* When the store list file is loaded in the folder containing the individual store
+				 * files will be in the same directory as it 
+				 */
+				storeCSVFileLocation = openFileDialog.InitialDirectory + @"/StoreData/";
+
 				string[] fileNames = Directory.GetFiles(openFileDialog.InitialDirectory + @"/StoreData");
-				foreach (var filePath in fileNames)
+				ConcurrentQueue<string> foo = new ConcurrentQueue<string>();
+
+				Stopwatch stopwatch = new Stopwatch();
+				stopwatch.Start();
+
+				ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+				string[] testOrderData = File.ReadAllLines(openFileDialog.InitialDirectory + @"/StoreData/ABE1_1_2013.csv");
+				Parallel.ForEach(testOrderData, data =>
 				{
-					string fileNameExt = Path.GetFileName(filePath);
-					string fileName = Path.GetFileNameWithoutExtension(filePath);
+					queue.Enqueue(data);
+				});
 
-					string[] fileNameSplit = fileName.Split('_');
-					Store store = Stores[fileNameSplit[0]];
-					Date date = new Date { week = Convert.ToInt32(fileNameSplit[1]), year = Convert.ToInt32(fileNameSplit[2]) };
-					dates.Add(date);
-
-					string[] orderData = File.ReadAllLines(openFileDialog.InitialDirectory + @"/StoreData/" + fileNameExt);
-					foreach (var orderInfo in orderData)
-					{
-						string[] orderSplit = orderInfo.Split(',');
-						Order order = new Order
-						{
-							store = store,
-
-							date = date,
-							supplier = orderSplit[0],
-							supplierType = orderSplit[1],
-							cost = Convert.ToDouble(orderSplit[2])
-						};
-
-						orders.Add(order);
-					}
+				foreach (var s in queue)
+				{
+					string[] orderSplit = s.Split(',');
 				}
 
 
 				stopwatch.Stop();
 				textBox1.Text += "Time to load: " + stopwatch.Elapsed;
+				richTextBox1.Text += "File count = " + fileCount + '\n';
+				richTextBox1.Text += "Queue count = " + foo.Count+ '\n';
+
+				/* Enable combo boxes for search queries*/
+				comboBox1.Enabled = true;
+				comboBox2.Enabled = true;
+				comboBox3.Enabled = true;
 			}
 		}
 
@@ -112,6 +135,38 @@ namespace TBSEAssessmentOne
 		private void richTextBox1_TextChanged(object sender, EventArgs e)
 		{
 			
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+
+			string fileToSearch = storeCSVFileLocation + comboBox1.Text + '_' + comboBox2.Text + '_' + comboBox3.Text + ".csv";
+			string[] storeData = File.ReadAllLines(fileToSearch);
+			ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+
+			Parallel.ForEach(storeData, data =>
+			{
+				queue.Enqueue(data);
+			});
+
+			if (dataGridView2.Rows.Count >= 2)
+			{
+				for (int i = 0; i < dataGridView2.Rows.Count - 1; i++)
+				{
+					dataGridView2.Rows.RemoveAt(i);
+				}
+			}
+
+			foreach (var s in queue)
+			{
+				string[] orderSplit = s.Split(',');
+				dataGridView2.Rows.Add(orderSplit[0], orderSplit[1], orderSplit[2]);
+			}
+
+			sw.Stop();
+			richTextBox1.Text += "Time: " + sw.Elapsed + '\n';
 		}
 	}
 }
